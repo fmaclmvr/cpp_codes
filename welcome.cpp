5cf966ff7a20b8ab69f40d6b0eaef0ddc7168f9b
James Guan <fmaclmvr@yahoo.com.tw>
2019-04-07 15:27:53 +0800

    add another line

--- welcome.cpp
+++ welcome.cpp
@@ -4,5 +4,6 @@ int main()
 {
     cout << "Welcome to the world of C++! " << endl;
     cout << "Today is 2019/4/7 " << endl;
+    cout << "I just learned how to use git on terminal!" << endl;
     return 0;
 }
